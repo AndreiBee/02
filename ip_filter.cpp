4e8f220b07c1f6aed666f769494848a432523601
AndreiBee <andrei-pchalavodau@seznam.cz>
2024-05-15 21:06:13 +0200

    Added tests

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -103,6 +103,136 @@ std::vector<std::string> filter_any(const std::vector<std::string> &ip_pool, con
     return r;
 }
 
+struct IPParser
+{
+    IPParser() = default;
+    IPParser(const std::string filePath) : m_FilePath(filePath), m_IsVectorSorted(false)
+    {
+        if (!std::filesystem::exists(m_FilePath))
+        {
+            std::cout << "Not a valid file name" << "\n";
+            return;
+        }
+
+        ParseFile(m_FilePath);
+    };
+
+    int SortLexicographically()
+    {
+        if (!m_IPPool.size())
+            return -1;
+        // Sort IP addresses lexicographically
+        std::sort(m_IPPool.begin(), m_IPPool.end(), compareIP);
+        m_IsVectorSorted = true;
+        return 0;
+    };
+
+    int ReverseVector()
+    {
+        if (!m_IPPool.size())
+            return -1;
+
+        if (!m_IsVectorSorted)
+            return -2;
+
+        // Reverse lexicographically sort
+        std::reverse(m_IPPool.begin(), m_IPPool.end());
+        m_IsVectorSorted = false;
+        return 0;
+    };
+
+    int FilterByFirstByte(const int byte)
+    {
+        if (!m_IPPool.size())
+            return -1;
+
+        auto filteredByFirstByteIP = filter(m_IPPool, byte);
+
+        if (!filteredByFirstByteIP.size())
+            return -1;
+
+        // Print the result
+        std::cout << "Filtered by the first byte: " << byte << "\n";
+        for (auto i{0}; i < filteredByFirstByteIP.size(); ++i)
+        {
+            std::cout << '\t' << filteredByFirstByteIP[i] << "\n";
+        }
+        return 0;
+    };
+
+    int FilterByFirstAndSecondBytes(const int firstByte, const int secondByte)
+    {
+        if (!m_IPPool.size())
+            return -1;
+
+        auto filteredByFirstAndSecondBytesIP = filter(m_IPPool, firstByte, secondByte);
+
+        if (!filteredByFirstAndSecondBytesIP.size())
+            return -1;
+
+        // Print the result
+        std::cout << "Filtered by the first: " << firstByte << " and second byte: " << secondByte << "\n";
+        for (auto i{0}; i < filteredByFirstAndSecondBytesIP.size(); ++i)
+        {
+            std::cout << '\t' << filteredByFirstAndSecondBytesIP[i] << "\n";
+        }
+        return 0;
+    };
+
+    int FilterByAnyByte(const int byte)
+    {
+        if (!m_IPPool.size())
+            return -1;
+
+        auto filteredByAnyByte = filter_any(m_IPPool, byte);
+
+        if (!filteredByAnyByte.size())
+            return -1;
+
+        // Print the result
+        std::cout << "Filtered by the byte: " << byte << "\n";
+        for (auto i{0}; i < filteredByAnyByte.size(); ++i)
+        {
+            std::cout << '\t' << filteredByAnyByte[i] << "\n";
+        }
+        return 0;
+    };
+
+private:
+    int ParseFile(const std::string filePath)
+    {
+        m_IPPool.clear();
+
+        std::ifstream inputFile(filePath);
+        // Check if the file opened successfully
+        if (!inputFile.is_open())
+        {
+            std::cerr << "Error: Unable to open file." << std::endl;
+            return -1; // Exit with error
+        }
+        // Read and print each line from the file
+        std::string line;
+        auto linesCount{0};
+        while (std::getline(inputFile, line))
+        {
+            std::vector<std::string> v = split(line, '\t');
+            m_IPPool.push_back(v.at(0));
+            ++linesCount;
+        }
+        std::cout << "Read " << linesCount << " lines of addresses." << "\n";
+
+        // Close the file
+        inputFile.close();
+
+        return 0;
+    };
+
+private:
+    std::string m_FilePath{};
+    std::vector<std::string> m_IPPool{};
+    bool m_IsVectorSorted{};
+};
+
 int main(int argc, char const *argv[])
 {
     if (argc == 1)
@@ -112,177 +242,12 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
-    if (!std::filesystem::exists(std::string(argv[1])))
-    {
-        std::cout << "Not a valid file name" << "\n";
-        return 1;
-    }
-
-    std::vector<std::string> ip_pool;
-
-    // Open and read the file
-    {
-        std::ifstream inputFile(argv[1]);
-        // Check if the file opened successfully
-        if (!inputFile.is_open())
-        {
-            std::cerr << "Error: Unable to open file." << std::endl;
-            return 1; // Exit with error
-        }
-        // Read and print each line from the file
-        std::string line;
-        auto linesCount{0};
-        while (std::getline(inputFile, line))
-        {
-            std::vector<std::string> v = split(line, '\t');
-            ip_pool.push_back(v.at(0));
-            ++linesCount;
-        }
-        std::cout << "Read " << linesCount << " lines of addresses." << "\n";
-
-        // Close the file
-        inputFile.close();
-    }
-
-    // Sort IP addresses lexicographically
-    std::sort(ip_pool.begin(), ip_pool.end(), compareIP);
-
-    // Reverse lexicographically sort
-    std::reverse(ip_pool.begin(), ip_pool.end());
-
-    {
-        // Filter by first byte and output
-        auto firstByte = 1;
-        auto filteredByFirstByteIP = filter(ip_pool, firstByte);
-
-        // Print the result
-        std::cout << "Filtered by the first byte: " << firstByte << "\n";
-        for (auto i{0}; i < filteredByFirstByteIP.size(); ++i)
-        {
-            std::cout << '\t' << filteredByFirstByteIP[i] << "\n";
-        }
-    }
-
-    {
-        // Filter by first and second bytes and output
-        auto firstByte = 46;
-        auto secondByte = 70;
-        auto filteredByFirstAndSecondBytesIP = filter(ip_pool, firstByte, secondByte);
-
-        // Print the result
-        std::cout << "Filtered by the first: " << firstByte << " and second byte: " << secondByte << "\n";
-        for (auto i{0}; i < filteredByFirstAndSecondBytesIP.size(); ++i)
-        {
-            std::cout << '\t' << filteredByFirstAndSecondBytesIP[i] << "\n";
-        }
-    }
-
-    {
-        // Filter by any byte and output
-        auto anyByte = 46;
-        auto filteredByAnyByte = filter_any(ip_pool, anyByte);
-
-        // Print the result
-        std::cout << "Filtered by the byte: " << anyByte << "\n";
-        for (auto i{0}; i < filteredByAnyByte.size(); ++i)
-        {
-            std::cout << '\t' << filteredByAnyByte[i] << "\n";
-        }
-    }
-    return 0;
-
-    try
-    {
-        std::vector<std::vector<std::string>> ip_pool;
-
-        for (std::string line; std::getline(std::cin, line);)
-        {
-            std::vector<std::string> v = split(line, '\t');
-            ip_pool.push_back(split(v.at(0), '.'));
-        }
-
-        // TODO reverse lexicographically sort
-
-        for (auto ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
-        {
-            for (auto ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
-            {
-                if (ip_part != ip->cbegin())
-                {
-                    std::cout << ".";
-                }
-                std::cout << *ip_part;
-            }
-            std::cout << std::endl;
-        }
-
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first byte and output
-        // ip = filter(1)
-
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // TODO filter by first and second bytes and output
-        // ip = filter(46, 70)
-
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-
-        // TODO filter by any byte and output
-        // ip = filter_any(46)
-
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
+    IPParser ipParser{std::string(argv[1])};
+    ipParser.SortLexicographically();
+    ipParser.ReverseVector();
+    ipParser.FilterByFirstByte(42);
+    ipParser.FilterByFirstAndSecondBytes(42, 128);
+    ipParser.FilterByAnyByte(28);
 
     return 0;
 }
