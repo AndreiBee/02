26e69d20a46033a7bc86165a4f411ee931bb2f52
andrei.pchalavodau <andrei.pchalavodau@rigaku.com>
2024-05-24 18:25:32 +0200

    Slightly changed release.yml file and uncommented the testing file.

--- tests/test_IPParser.cpp
+++ tests/test_IPParser.cpp
@@ -15,10 +15,10 @@ BOOST_AUTO_TEST_CASE(test_parser)
     BOOST_CHECK(ipParser.FilterByFirstAndSecondBytes(28, 256) == -1);
     BOOST_CHECK(ipParser.FilterByAnyByte(28) == -1);
 
-    // IPParser ipParser2{std::string("../ip_filter.tsv")};
-    // BOOST_CHECK(ipParser2.SortLexicographically() == 0);
-    // BOOST_CHECK(ipParser2.ReverseVector() == 0);
-    // BOOST_CHECK(ipParser2.FilterByFirstByte(42) == 0);
-    // BOOST_CHECK(ipParser2.FilterByFirstAndSecondBytes(42, 54) == 0);
-    // BOOST_CHECK(ipParser2.FilterByAnyByte(28) == 0);
+    IPParser ipParser2{std::string("../ip_filter.tsv")};
+    BOOST_CHECK(ipParser2.SortLexicographically() == 0);
+    BOOST_CHECK(ipParser2.ReverseVector() == 0);
+    BOOST_CHECK(ipParser2.FilterByFirstByte(42) == 0);
+    BOOST_CHECK(ipParser2.FilterByFirstAndSecondBytes(42, 54) == 0);
+    BOOST_CHECK(ipParser2.FilterByAnyByte(28) == 0);
 }
